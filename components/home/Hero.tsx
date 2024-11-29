76c5869729aa97ed3a61d717b93dbb334cfd8aea
subject026 <subject026@protonmail.com>
2024-11-29 16:34:18 +0000

    overflow issues

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="text-center max-w-3xl relative mx-auto flex flex-col items-center pt-6 md:pt-12 lg:pt-24">
+    <section className="page-wrap text-center max-w-3xl relative mx-auto flex flex-col items-center pt-6 md:pt-12 lg:pt-24">
       <h1 className="font-header text-5xl md:text-6xl lg:text-7xl leading-[64px] md:leading-[72px] lg:leading-[84px] font-extrabold">
         A <span className="text-primary">funding platform</span> for
         community-led sports projects.
@@ -13,7 +13,7 @@ export default function Hero() {
         We&apos;re starting a new public goods funding initiative to support
         projects that advance inclusion and empowerment through sport.
       </p>
-      <div className="mt-12 min-w-[400px] flex flex-col sm:flex-row gap-2.5">
+      <div className="mt-12 w-full flex flex-col sm:flex-row gap-2.5">
         <Link href="/projects">
           <PrimaryButton icon={<ArrowRight />}>Our Projects</PrimaryButton>
         </Link>

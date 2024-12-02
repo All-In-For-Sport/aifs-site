7065006e753e7d35e3953e831dbe7fe0cdd2769b
subject026 <subject026@protonmail.com>
2024-12-02 14:10:48 +0000

    render 404 for project/[slug]

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { mockProject } from "@/app/mockProjects";
+// import { mockProject } from "@/app/mockProjects";
 import {
   ProjectAbout,
   ProjectCTA,
@@ -6,31 +6,33 @@ import {
   ProjectGoals,
   ProjectHero,
 } from "@/components/project";
+import { notFound } from "next/navigation";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  return {
-    title: `${mockProject.name} | All in for Sport`,
-    description: mockProject.description,
-  };
-}
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: { slug: string };
+// }) {
+//   return {
+//     title: `${mockProject.name} | All in for Sport`,
+//     description: mockProject.description,
+//   };
+// }
 
 export default async function ProjectPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-12 lg:p-24">
-      <div className="block left-0 right-0 mx-auto w-[400px] h-[300px] rounded-full -z-20 bg-[#5A6DED] blur-[100px] absolute -top-[150px]" />
-      <ProjectHero project={mockProject} />
-      <ProjectAbout project={mockProject} />
-      <ProjectDetails project={mockProject} />
-      <ProjectGoals project={mockProject} />
-      {mockProject.isActive && <ProjectCTA project={mockProject} />}
-    </main>
-  );
+  return notFound();
+  // return (
+  //   <main className="flex min-h-screen flex-col items-center justify-between p-6 md:p-12 lg:p-24">
+  //     <div className="absolute -top-[150px] left-0 right-0 -z-20 mx-auto block h-[300px] w-[400px] rounded-full bg-[#5A6DED] blur-[100px]" />
+  //     <ProjectHero project={mockProject} />
+  //     <ProjectAbout project={mockProject} />
+  //     <ProjectDetails project={mockProject} />
+  //     <ProjectGoals project={mockProject} />
+  //     {mockProject.isActive && <ProjectCTA project={mockProject} />}
+  //   </main>
+  // );
 }

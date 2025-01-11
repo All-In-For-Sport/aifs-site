4d0fd4627fdecad919368155fe636760e2046792
subject026 <subject026@protonmail.com>
2025-01-11 12:52:49 +0000

    tag navigation styling

--- app/updates/components/TagNavigation.tsx
+++ app/updates/components/TagNavigation.tsx
@@ -2,50 +2,38 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
-import { ReactNode, useCallback } from "react";
 
-export function TagNavigation() {
-  const pathname = usePathname();
-
+export function TagNavigation({ tags }: { tags: Array<string> }) {
   return (
     <nav className="flex gap-4">
-      <TagLink href={pathname}>All</TagLink>
-      <TagLink href={pathname} postType="article">
-        Articles
-      </TagLink>
-
-      <TagLink href={pathname} postType="event">
-        Events
-      </TagLink>
+      <TagLink tag="">All</TagLink>
+      {tags.map((tag) => (
+        <TagLink key={`tag_${tag}`} tag={tag}>
+          {tag.slice(0, 1).toUpperCase() + tag.slice(1, tag.length) + "s"}
+        </TagLink>
+      ))}
     </nav>
   );
 }
 
-function TagLink({
-  children,
-  href,
-  postType,
-}: {
-  children: ReactNode;
-  href: string;
-  postType?: string;
-}) {
+function TagLink({ children, tag }: { children: string; tag: string }) {
+  const pathname = usePathname();
   const searchParams = useSearchParams();
 
   const currentPostType = searchParams.get("type");
+
   return (
     <Link
-      href={postType ? href + "?" + "type=" + postType : href}
+      href={tag ? pathname + "?" + "type=" + tag : pathname}
       className={clsx(
-        "rounded-full px-4 py-1.5 font-bold",
-        !currentPostType && !postType
-          ? highlightClasses
-          : postType === currentPostType && highlightClasses,
+        "border-b-[3px] border-[#00000000] border-opacity-0 px-4 py-1.5 font-bold text-neutral-400 transition-all hover:text-neutral-200",
+        !currentPostType && !tag
+          ? "border-primary border-opacity-100 text-neutral-200"
+          : tag === currentPostType &&
+              "border-primary border-opacity-100 text-neutral-200",
       )}
     >
       {children}
     </Link>
   );
 }
-
-const highlightClasses = "bg-primary text-neutral-800";

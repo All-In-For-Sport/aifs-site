857d53d4cb7a89488e0af3da4530bc909d5627e1
subject026 <subject026@protonmail.com>
2025-01-22 19:43:38 +0000

    update next config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,16 @@ const withExportImages = require("next-export-optimize-images");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { hostname: "images.unsplash.com" },
       { hostname: "cdn.sanity.io" },
       { hostname: "arweave.net" },
     ],
   },
+  trailingSlash: true,
 };
 
 module.exports = withExportImages(withContentlayer(nextConfig));

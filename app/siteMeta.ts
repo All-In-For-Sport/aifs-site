d924acd0953893ad9608518af983612397c200b6
subject026 <subject026@protonmail.com>
2025-01-27 19:23:01 +0000

    update badges link

--- app/siteMeta.ts
+++ app/siteMeta.ts
@@ -4,6 +4,7 @@ export const siteLinks = {
   discordJoin: "https://discord.com/invite/HyeK5hf4vR",
   clarity: "https://app.clarity.so/allinforsport",
   discuss: "https://discuss.allinforsport.org",
+  hats: "https://app.hatsprotocol.xyz/trees/10/78",
   opensea: "https://opensea.io/collection/all-in-for-sport",
   snapshot: "https://snapshot.org/#/allinforsport.eth",
   state: "https://state.allinforsport.org/",

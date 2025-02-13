bd558549822526872cba44333e3ca5b83e4d1bf9
subject026 <subject026@protonmail.com>
2025-02-13 13:24:14 +0000

    add video component & update readme

--- app/updates/components/MDXContent.tsx
+++ app/updates/components/MDXContent.tsx
@@ -1,6 +1,8 @@
+import { BonfireVideo } from "@/app/mdx/BonfireVideo";
 import { useMDXComponent } from "next-contentlayer2/hooks";
 
 export function MDXContent({ mdx }: { mdx: string }) {
   const MDXContent = useMDXComponent(mdx);
-  return <MDXContent />;
+
+  return <MDXContent components={{ BonfireVideo }} />;
 }
